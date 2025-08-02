a62e531f301c4e9feab8db04563f98283c3faae1
InanChan <n.grishanenko98@mail.ru>
2025-08-02 13:23:50 +0700

    change eltex3-1.c

--- 03/eltex3-1.c
+++ 03/eltex3-1.c
@@ -25,17 +25,3 @@ int main(void) {
 
     return 0;
 }
-
-// #include <stdio.h>
-
-// int main() {
-//     int a, number;
-//     scanf("%d%d", &a, &number);
-//     unsigned char *ptr = (char*) &a;
-//     for(int i = 1; i <= sizeof(a); i++){
-//         *ptr = i == 3 ? number : *ptr;
-//         printf("%d ", *ptr++);
-//     }
-
-//     return 0;
-// }
